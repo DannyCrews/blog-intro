2877a95dc022207971ce52ba50dedc239f101cc3
dannycrews <dancrews@me.com>
2017-04-26 15:57:41 -0400

    Enable post editor

--- src/admin/enable-editor.php
+++ src/admin/enable-editor.php
@@ -11,3 +11,19 @@
  */
 
 namespace BlogIntro;
+
+add_action('edit_form_after_title', __NAMESPACE__ . '\enable_page_for_posts_editor' );
+/**
+ * Add the posts editor to the page_for_posts page
+ *
+ * @since 1.0.0
+ *
+ * @param \WP_Post $post Current Post Object
+ *
+ * @return void
+ */
+function enable_page_for_posts_editor( $post ) {
+	if ( get_option( 'page_for_posts' ) === $post->ID ) {
+		add_post_type_support( 'page', 'editor' );
+	}
+}
